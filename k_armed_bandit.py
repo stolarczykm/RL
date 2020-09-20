75c7277bce46b13b2d7c2b55297fbc7a06941e40
Michal Stolarczyk <michal.stolarczyk0@gmail.com>
2020-09-20 22:16:30 +0200

    Added gradient and upper confidence bound policies

--- k_armed_bandit.py
+++ k_armed_bandit.py
@@ -1,5 +1,8 @@
-import numpy as np
+import abc
+from typing import Callable, List
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class KArmedBandit():
@@ -9,7 +12,8 @@ class KArmedBandit():
         self._means = self._random_state.normal(0, 1, size=k)
 
     def __call__(self, action: int):
-        assert 0 <= action < self._k
+        if not 0 <= action < self._k:
+            raise ValueError(f"action is {action}, should be between 0 and {self._k - 1} (inclusive)")
         return self._random_state.normal(self._means[action], 1)
 
     @property
@@ -17,107 +21,161 @@ class KArmedBandit():
         return self._k 
 
 
+class Policy(abc.ABC):
+    @abc.abstractmethod
+    def choose_action(self, t: int):
+        pass
 
-class GreedyMethod():
-    def __init__(self, bandit: KArmedBandit):
-        self._bandit = bandit 
-        self._rewards = np.zeros(bandit.k, dtype=np.float)
-        self._counts = np.zeros(bandit.k, dtype=np.int)
-        self._q = np.zeros(bandit.k, dtype=np.float)
+    @abc.abstractmethod
+    def update_state(self, t, action, reward):
+        pass
 
-    def choose_action(self):
+    def run(self, bandit: KArmedBandit, iterations=1000):
+        rewards = []
+        for t in range(1, iterations + 1):
+            action  = self.choose_action(t)
+            reward = bandit(action)
+            rewards.append(reward)
+            self.update_state(t, action, reward)
+        return rewards
+
+
+class SampleAverageValuePolicyMixin:
+    def _init_arrays(self, space_size):
+        self._rewards = np.zeros(space_size, dtype=np.float)
+        self._counts = np.zeros(space_size, dtype=np.int)
+        self._q = np.zeros(space_size, dtype=np.float)
+    
+    def _update_state(self, t, action, reward):
+        self._rewards[action] += reward
+        self._counts[action] += 1
+        self._q[action] = self._rewards[action] / self._counts[action] 
+
+
+class GreedyMethod(Policy, SampleAverageValuePolicyMixin):
+    def __init__(self, space_size: int):
+        self._init_arrays(space_size)
+
+    def choose_action(self, t: int):
         return np.argmax(self._q)
     
-    def update_state(self, action, reward):
-        self._rewards[action] += reward
-        self._counts[action] += 1
-        self._q[action] = self._rewards[action] / self._counts[action] 
-    
-    def run(self, iterations=1000):
-        rewards = []
-        for i in range(iterations):
-            action = self.choose_action()
-            reward = self._bandit(action)
-            rewards.append(reward)
-            self.update_state(action, reward)
-        return rewards
+    def update_state(self, t, action, reward):
+        self._update_state(t, action, reward)
 
 
-class EpsilonGreedyMethod():
-    def __init__(self, bandit: KArmedBandit, epsilon: float, seed: int = 123):
-        self._bandit = bandit 
+class EpsilonGreedyMethod(Policy, SampleAverageValuePolicyMixin):
+    def __init__(self, space_size: int, epsilon: float, seed: int = 123):
+        self._space_size = space_size
         self._epsilon = epsilon
         self._random_state = np.random.RandomState(seed)
-        self._rewards = np.zeros(bandit.k, dtype=np.float)
-        self._counts = np.zeros(bandit.k, dtype=np.int)
-        self._q = np.zeros(bandit.k, dtype=np.float)
+        self._init_arrays(space_size)
 
-    def choose_action(self):
+    def choose_action(self, t):
         u = self._random_state.uniform()
-        return np.argmax(self._q) if u > self._epsilon else self._random_state.randint(self._bandit.k)
+        return np.argmax(self._q) if u > self._epsilon else self._random_state.randint(self._space_size)
     
-    def update_state(self, action, reward):
-        self._rewards[action] += reward
-        self._counts[action] += 1
-        self._q[action] = self._rewards[action] / self._counts[action] 
-    
-    def run(self, iterations=1000):
-        rewards = []
-        for i in range(iterations):
-            action = self.choose_action()
-            reward = self._bandit(action)
-            rewards.append(reward)
-            self.update_state(action, reward)
-        return rewards
+    def update_state(self, t, action, reward):
+        self._update_state(t, action, reward)
 
 
-class EpsilonGreedyMethodWithDecay():
-    def __init__(self, bandit: KArmedBandit, epsilon: float, epsilon_decay: float, seed: int = 123):
-        self._bandit = bandit 
+class EpsilonGreedyMethodWithDecay(Policy, SampleAverageValuePolicyMixin):
+    def __init__(self, space_size: int, epsilon: float, epsilon_decay: float, seed: int = 123):
+        self._space_size = space_size
         self._epsilon = epsilon
         self._random_state = np.random.RandomState(seed)
-        self._rewards = np.zeros(bandit.k, dtype=np.float)
-        self._counts = np.zeros(bandit.k, dtype=np.int)
-        self._q = np.zeros(bandit.k, dtype=np.float)
         self._epsilon_decay = epsilon_decay
+        self._init_arrays(space_size)
 
-    def choose_action(self):
+    def choose_action(self, t: int):
         u = self._random_state.uniform()
-        res =  np.argmax(self._q) if u > self._epsilon else self._random_state.randint(self._bandit.k)
+        res =  np.argmax(self._q) if u > self._epsilon else self._random_state.randint(self._space_size)
         self._epsilon *= self._epsilon_decay
         return res
     
-    def update_state(self, action, reward):
-        self._rewards[action] += reward
-        self._counts[action] += 1
-        self._q[action] = self._rewards[action] / self._counts[action] 
+    def update_state(self, t, action, reward):
+        self._update_state(t, action, reward)
     
-    def run(self, iterations=1000):
-        rewards = []
-        for i in range(iterations):
-            action = self.choose_action()
-            reward = self._bandit(action)
-            rewards.append(reward)
-            self.update_state(action, reward)
-        return rewards
+
+class UpperConfidenceBoundPolicy(Policy, SampleAverageValuePolicyMixin):
+    def __init__(self, space_size: int, c: float, seed: int = 123):
+        self._space_size = space_size
+        self._c = c
+        self._init_arrays(space_size)
+
+    def choose_action(self, t: int):
+        if t < 1:
+            raise ValueError("t can't be smaller than 1")
+        values = self._q + self._c * np.sqrt(np.log(t) / (self._counts + 1e-6))
+        return np.argmax(np.where(self._counts == 0, np.inf, values))
+    
+    def update_state(self, t, action, reward):
+        self._update_state(t, action, reward)
 
 
+def softmax(x: np.ndarray):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+    
+
+class GradientPolicy(Policy):
+    def __init__(self, space_size: int, step_size: float, seed: int = 123):
+        self._step_size = step_size
+        self._space_size = space_size
+        self._h = np.zeros(space_size, dtype=np.float)
+        self._baseline: float = 0.0
+        self._random_state = np.random.RandomState(seed)
+    
+    def choose_action(self, t: int):
+        probs = softmax(self._h)
+        return self._random_state.choice(len(probs), p=probs)
+
+    def update_state(self, t, action, reward):
+        signs = -np.ones(self._space_size, dtype=np.float)
+        signs[action] = 1.0
+
+        probs = softmax(self._h)
+        probs[action] = 1 - probs[action]
+        
+        self._h = self._h + signs * self._step_size * (reward - self._baseline) * probs
+        self._baseline = self._baseline + (1 / t) * (reward - self._baseline)
+
+
+def plot_results(rewards, policies: List[Callable[[int], Policy]]):
+    mean_rewards = np.array(rewards).mean(axis=0)
+
+    plt.figure(figsize=(14, 8))
+    for rewards, policy in zip(mean_rewards, policies): 
+        plt.plot(rewards, alpha=0.4, label=policy(1).__name__)
+    plt.xlabel("Iteration")
+    plt.ylabel("Average reward")
+    plt.legend()
+    plt.show()
+
+
+def run_simulations(space_size, policies, n_seeds, n_iterations):
+    rewards = []
+    for seed in range(n_seeds): 
+        rewards.append([])
+        for policy_func in policies:
+            bandit = KArmedBandit(space_size, seed=seed)
+            res = policy_func(seed).run(bandit, n_iterations)
+            rewards[-1].append(res)
+    return rewards
+
+def main():
+    space_size = 10
+    policies: List[Callable[[int], Policy]] = [
+        lambda seed: GreedyMethod(space_size),
+        lambda seed: EpsilonGreedyMethod(space_size, 0.05, seed=seed),
+        lambda seed: EpsilonGreedyMethodWithDecay(space_size, 0.1, 0.998, seed=seed),
+        lambda seed: UpperConfidenceBoundPolicy(space_size, 1.0, seed=seed),
+        lambda seed: GradientPolicy(space_size, 0.1, seed=seed),
+    ]
+    rewards = run_simulations(space_size, policies, 100, 1000)
+    plot_results(rewards, policies)
 
 if __name__ == "__main__":
-    sums = []
-    for seed in range(100): 
-        bandit = KArmedBandit(10, seed=seed)
-        greedy = GreedyMethod(bandit)
-        rewards = greedy.run(1000)
+    main()
 
-        bandit = KArmedBandit(10, seed=seed)
-        eps_greedy = EpsilonGreedyMethod(bandit, 0.05, seed=seed)
-        rewards_eps = eps_greedy.run(1000)
 
-        bandit = KArmedBandit(10, seed=seed)
-        eps_greedy_2 = EpsilonGreedyMethodWithDecay(bandit, 0.1, 0.998, seed=seed)
-        rewards_eps_2 = eps_greedy_2.run(1000)
-        sums.append([sum(rewards), sum(rewards_eps), sum(rewards_eps_2)])
-    
-    print(np.mean(sums, axis=0))
 
